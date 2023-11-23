72928071fffd3f7eaf40f19727694c625d1c9d6f
Mitchell Davis <mdavis@atlas.dev>
2023-11-23 15:01:26 +1100

    Add phpunit.xml and remove workbench trait.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,7 @@
 
 namespace Atlas\LaravelAustralianSuperannuationFunds\Tests;
 
-use Orchestra\Testbench\Concerns\WithWorkbench;
-
 class TestCase extends \Orchestra\Testbench\TestCase
 {
-    use WithWorkbench;
-
     //
 }
